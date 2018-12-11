8559417f24cf484800fcd78561322aeb58f1c79d
Vivek Teega <vivek.teega@gmail.com>
2018-12-11 07:07:51 +0530

    Transaction page completed

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import SearchForm, BlankForm
 import sqlite3
@@ -29,9 +29,10 @@ def index():
         c.execute('SELECT address,SUM(transferBalance) FROM transactiontable GROUP BY address')
         balanceTable = c.fetchall()
         conn.close()
-        return render_template('test.html', form=form, blankform=blankform, balance=balance, transactionHistoryTable=transactionHistoryTable, balanceTable=balanceTable)
+        return render_template('index.html', form=form, blankform=blankform, balance=balance, transactionHistoryTable=transactionHistoryTable, balanceTable=balanceTable)
 
     if blankform.validate_on_submit():
+        return "str"
         flash('Balance requested for address {}'.format(form.address.data))
         conn = sqlite3.connect(dblocation)
         c = conn.cursor()
@@ -42,6 +43,23 @@ def index():
         c.execute('SELECT address,SUM(transferBalance) FROM transactiontable GROUP BY address')
         balanceTable = c.fetchall()
         conn.close()
-        return render_template('test.html', form=form, blankform=blankform, balance=balance, transactionHistoryTable=transactionHistoryTable, balanceTable=balanceTable)
+        return render_template('index.html', form=form, blankform=blankform, balance=balance, transactionHistoryTable=transactionHistoryTable, balanceTable=balanceTable)
 
     return render_template('index.html', form=form, blankform=blankform, transactionHistoryTable=transactionHistoryTable, balanceTable=balanceTable)
+
+@app.route('/transactions', methods=['GET', 'POST'])
+def transactions():
+    address = request.args.get('address')
+    if address is not None:
+        conn = sqlite3.connect(dblocation)
+        c = conn.cursor()
+        c.execute('SELECT * FROM transactionHistory WHERE fromAddress="{}" OR toAddress="{}" ORDER BY id DESC'.format(address,address))
+        transactionHistoryTable = c.fetchall()
+        conn.close()
+        return render_template('transactions.html', transactionHistoryTable=transactionHistoryTable)
+    conn = sqlite3.connect(dblocation)
+    c = conn.cursor()
+    c.execute('SELECT * FROM transactionHistory ORDER BY id DESC')
+    transactionHistoryTable = c.fetchall()
+    conn.close()
+    return render_template('transactions.html', transactionHistoryTable=transactionHistoryTable)
